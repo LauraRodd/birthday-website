6b494a9447a492bf2874daa77800447df5c819fe
Laura Rodd <laura_rodd@hotmail.com>
2020-09-08 13:43:17 +0200

    initial deploy commit

---
+++ app.js
@@ -0,0 +1,68 @@
+const { urlencoded } = require('body-parser');
+
+const bodyParser = require('body-parser'),
+	mongoose = require('mongoose'),
+	express = require('express'),
+	app = express(),
+	birthday = require('./views/script');
+
+// APP CONFIG
+
+mongoose.connect('mongodb://localhost/bday_v3', { useNewUrlParser: true, useUnifiedTopology: true });
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+// MONGOOSE/MODEL CONFIG
+
+const messageSchema = new mongoose.Schema({
+	name: String,
+	body: String,
+	created: { type: Date, default: Date.now }
+});
+const Message = mongoose.model('Message', messageSchema);
+
+// RESTFUL ROUTES
+
+// index
+
+app.get('/', function(req, res) {
+	res.redirect('/home');
+});
+
+// Comments Routes
+
+app.get('/home', function(req, res) {
+	Message.find({}, function(err, messages) {
+		if (err) {
+			console.log('Error');
+		} else {
+			res.render('index', { messages: messages });
+		}
+	});
+});
+
+// new comments route
+app.get('/home/new', function(req, res) {
+	res.render('new');
+});
+
+// create a new comment
+app.post('/home', function(req, res) {
+	Message.create(req.body.message, function(err, newMessage) {
+		if (err) {
+			res.render('new');
+		} else {
+			res.redirect('/home#messages');
+		}
+	});
+});
+
+// SERVER LISTEN
+// app.listen(3000, function() {
+// 	console.log('Server Has Started!');
+// });
+
+app.listen(process.env.PORT, process.env.IP, function() {
+	console.log('Kamil Birthday Server Has Started!');
+});
---
+++ views/script.js
@@ -0,0 +1,110 @@
+var express = require('express');
+var app = express();
+
+// confetti background animation
+
+// forms validation
+
+// First Form
+
+// function displayAnswerOne() {
+// 	let celeb = document.getElementsByName('celebration');
+
+// 	for (i = 0; i < celeb.length; i++) {
+// 		if (celeb[i].checked)
+// 			document.getElementById('celebrationResult').innerHTML =
+// 				'Your Celebration coupon is now active with: <br><strong> ' + celeb[i].value + '</strong>';
+// 	}
+// }
+
+// let formOne = document.getElementById('firstForm');
+// function handleFormOne(event) {
+// 	event.preventDefault();
+// }
+
+// if (formOne) {
+// 	formOne.addEventListener('submit', handleFormOne, false);
+// }
+
+// // Second form
+
+// function displayAnswerTwo() {
+// 	let food = document.getElementsByName('meal');
+
+// 	for (i = 0; i < food.length; i++) {
+// 		if (food[i].checked)
+// 			document.getElementById('mealResult').innerHTML =
+// 				'Your Special Meal coupon is now active with: <br><strong> ' + food[i].value + '</strong>';
+// 	}
+// }
+
+// let formTwo = document.getElementById('secondForm');
+// function handleFormTwo(event) {
+// 	event.preventDefault();
+// }
+
+// if (formTwo) {
+// 	formTwo.addEventListener('submit', handleFormTwo, false);
+// }
+
+// // Third Form
+
+// function displayAnswerThree() {
+// 	let getaway = document.getElementsByName('getaway');
+
+// 	for (i = 0; i < getaway.length; i++) {
+// 		if (getaway[i].checked)
+// 			document.getElementById('getawayResult').innerHTML =
+// 				'Your Romantic Getaway coupon is now active with: <br><strong> ' + getaway[i].value + '</strong>';
+// 	}
+// }
+
+// let formThree = document.getElementById('thirdForm');
+// function handleFormThree(event) {
+// 	event.preventDefault();
+// }
+// if (formThree) {
+// 	formThree.addEventListener('submit', handleFormThree, false);
+// }
+
+// // Fourth Form
+
+// function displayAnswerFour() {
+// 	let night = document.getElementsByName('night');
+
+// 	for (i = 0; i < night.length; i++) {
+// 		if (night[i].checked)
+// 			document.getElementById('nightResult').innerHTML =
+// 				'Your Night Out coupon is now active with: <br><strong> ' + night[i].value + '</strong>';
+// 	}
+// }
+
+// let formFour = document.getElementById('fourthForm');
+// function handleFormFour(event) {
+// 	event.preventDefault();
+// }
+
+// if (formFour) {
+// 	formFour.addEventListener('submit', handleFormFour, false);
+// }
+
+// // Fifth Form
+
+// function displayAnswerFive() {
+// 	let movie = document.getElementsByName('movie');
+
+// 	for (i = 0; i < movie.length; i++) {
+// 		if (movie[i].checked)
+// 			document.getElementById('movieResult').innerHTML =
+// 				'Your Movie Night coupon is now active with:<br><strong> ' + movie[i].value + '</strong>';
+// 	}
+// }
+
+// let formFive = document.getElementById('fifthForm');
+// function handleFormFive(event) {
+// 	event.preventDefault();
+// }
+
+// if (formFive) {
+// 	formFive.addEventListener('submit', handleFormFive, false);
+// }
